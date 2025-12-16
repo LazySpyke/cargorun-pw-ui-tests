371b8c564e2a9b905c4f9b0c7334203643fe5027
Lenard <lazyspyke3@gmail.com>
2025-12-16 18:16:20 +0300

    Добавил тесты по транзакции и по получение данных файлов в чатах

---
+++ tests/SystemApi/GetFileAttachments.spec.ts
@@ -0,0 +1,155 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../pages/LoginPage';
+import { getAuthData } from '../../database';
+import { BidCreateInfo } from '../../pages/Fixtures';
+import moment from 'moment';
+import APIRequestsClient from '../../api/clienApiRequsets';
+import APIBid from '../../api/bidApi';
+import SupportAPIRequestsClient from '../../api/testSupportRequsets'
+const clienApi = new APIRequestsClient();
+const bidApi = new APIBid();
+const emulatorApi = new SupportAPIRequestsClient();
+let bidInfo: any;
+const adminId = 427
+test.describe('Запрос GetFileAttachments', () => {
+    let loginPage: LoginPage;
+    let bidResponse: any;
+    test.beforeEach(async ({ page }) => {
+        loginPage = new LoginPage(page);
+        await loginPage.goto(); // Переходим на страницу логина перед каждым тестом
+    });
+
+    test('Запрос GetFileAttachments с удалённым файлом и не удалённым по чату уведомлений', async ({ page }) => {
+        await test.step('Логин', async () => {
+            await loginPage.login(process.env.logistEmail as string, process.env.logistPassword as string);
+        });
+        await test.step('Создание заявки и запуск в работу', async () => {
+            const bidFixture = new BidCreateInfo(page);
+            bidInfo = await bidFixture.ApiCommonBid({
+                price: 100000,
+                paymentTypeId: 176,
+                ndsTypeId: 175,
+                planEnterLoadDate: moment().subtract(6, 'h').format('YYYY-MM-DDTHH:mm'),
+                planEnterUnloadDate: moment().subtract(1, 'h').format('YYYY-MM-DDTHH:mm'),
+                loadAddress: 'Челны',
+                unloadAddress: 'Уфа',
+                userIdForFilter: adminId
+            });
+            await bidApi.init();
+            const bidList = await clienApi.GetObjectResponse(
+                `${process.env.url}/api/bids/getlist?$filter=carIds/any(carids:carids in (${bidInfo.carOption.carId}))and ((((status in ('Started')) or (status in ('Planned')))))&$orderby=id desc&$top=30&$skip=0`,
+                await getAuthData(adminId)
+            );
+            bidList.forEach(async (element: any) => {
+                bidApi.cancelBid(element.id, await getAuthData(adminId));
+            });
+            bidResponse = await bidApi.apply(bidInfo, await getAuthData(adminId));
+            await bidApi.setStatus(bidResponse.id, await getAuthData(adminId));
+        });
+        await test.step('Добавление файлов и удаление', async () => {
+            await page.goto(`${process.env.url}/bids/bid/${bidResponse.id}`);
+            await page.locator('[class="notifys notifys__bell"]').click();
+            await page.locator('[class="chat-footer__file"]').click();
+            await page.locator('input[type="file"]').setInputFiles('C:/cargorun-pw-ui-tests/LenardMem.png')
+            await page.locator('[class="btn btn-brand btn-sm modal-window__footer-action"]').click();
+            await expect(page.locator('[title="LenardMem.png"]')).toBeVisible();
+
+            await page.locator('[class="chat-msg__delete-btn"]').click();
+            await page.locator('[class="btn btn-brand btn-sm modal-window__footer-action"]').click();
+            await expect(page.locator('[title="LenardMem.png"]')).not.toBeVisible();
+            await expect(page.getByText('Ваш запрос выполнен успешно.')).toBeVisible();
+
+            await page.locator('[class="chat-footer__file"]').click();
+            await page.locator('input[type="file"]').setInputFiles('C:/cargorun-pw-ui-tests/Alfir.jpg')
+            await page.locator('[class="btn btn-brand btn-sm modal-window__footer-action"]').click();
+            await expect(page.locator('[title="Alfir.jpg"]')).toBeVisible();
+        })
+        await test.step('Проверка запроса GetFileAttachments', async () => {
+            const getFileAttachmentsList = await clienApi.GetObjectResponse(
+                `${process.env.url}/api/Bids/GetFileAttachments/${bidResponse.id}`,
+                await getAuthData(adminId))
+            console.log(getFileAttachmentsList)
+            if (getFileAttachmentsList.length > 1) {
+                throw new Error(`количество вернувшихся файлов не корректное`)
+            }
+            const getFileAttachmentsListAllFiles = await clienApi.GetObjectResponse(
+                `${process.env.url}/api/Bids/GetFileAttachments/${bidResponse.id}?WithDeleted=true`,
+                await getAuthData(adminId))
+            console.log(getFileAttachmentsListAllFiles)
+            if (getFileAttachmentsListAllFiles.length <= 1) {
+                throw new Error(`количество вернувшихся файлов не корректное`)
+            }
+        })
+    })
+    test('Запрос GetFileAttachments с удалённым файлом и не удалённым по чату водителей', async ({ page }) => {
+        await test.step('Логин', async () => {
+            await loginPage.login(process.env.logistEmail as string, process.env.logistPassword as string);
+        });
+        await test.step('Создание заявки и запуск в работу', async () => {
+            const bidFixture = new BidCreateInfo(page);
+            bidInfo = await bidFixture.ApiCommonBid({
+                price: 100000,
+                paymentTypeId: 176,
+                ndsTypeId: 175,
+                planEnterLoadDate: moment().subtract(6, 'h').format('YYYY-MM-DDTHH:mm'),
+                planEnterUnloadDate: moment().subtract(1, 'h').format('YYYY-MM-DDTHH:mm'),
+                loadAddress: 'Челны',
+                unloadAddress: 'Уфа',
+                userIdForFilter: adminId
+            });
+            await bidApi.init();
+            const bidList = await clienApi.GetObjectResponse(
+                `${process.env.url}/api/bids/getlist?$filter=carIds/any(carids:carids in (${bidInfo.carOption.carId}))and ((((status in ('Started')) or (status in ('Planned')))))&$orderby=id desc&$top=30&$skip=0`,
+                await getAuthData(adminId)
+            );
+            bidList.forEach(async (element: any) => {
+                bidApi.cancelBid(element.id, await getAuthData(adminId));
+            });
+            bidResponse = await bidApi.apply(bidInfo, await getAuthData(adminId));
+            await bidApi.setStatus(bidResponse.id, await getAuthData(adminId));
+        });
+        await test.step('Добавление файлов и удаление', async () => {
+            await page.goto(`${process.env.url}/bids/bid/${bidResponse.id}`);
+            await page.locator("//a[@class='show-bid__link d-inline-block position-relative']").click();
+            await page.locator('[class="chat-footer__file"]').click();
+            await page.locator('input[type="file"]').setInputFiles('C:/cargorun-pw-ui-tests/LenardMem.png')
+            await page.locator('[class="btn btn-brand btn-sm modal-window__footer-action"]').click();
+            await expect(page.locator('[title="LenardMem.png"]')).toBeVisible();
+
+            await page.locator('[class="chat-msg__actions"]').click();
+            await page.getByText('Удалить').click();
+            await page.locator('[class="btn btn-brand btn-sm modal-window__footer-action"]').click();
+            await expect(page.locator('[title="LenardMem.png"]')).not.toBeVisible();
+            await expect(page.getByText('Ваш запрос выполнен успешно.')).toBeVisible();
+
+            await page.locator('[class="chat-footer__file"]').click();
+            await page.locator('input[type="file"]').setInputFiles('C:/cargorun-pw-ui-tests/Alfir.jpg')
+            await page.locator('[class="btn btn-brand btn-sm modal-window__footer-action"]').click();
+            await expect(page.locator('[title="Alfir.jpg"]')).toBeVisible();
+        })
+        await test.step('Проверка запроса GetFileAttachments', async () => {
+            const getFileAttachmentsList = await clienApi.GetObjectResponse(
+                `${process.env.url}/api/Bids/GetFileAttachments/${bidResponse.id}?ChatType=Secondary`,
+                await getAuthData(adminId))
+            console.log(getFileAttachmentsList)
+            if (getFileAttachmentsList.length > 1) {
+                throw new Error(`количество вернувшихся файлов не корректное`)
+            }
+            const getFileAttachmentsListAllFiles = await clienApi.GetObjectResponse(
+                `${process.env.url}/api/Bids/GetFileAttachments/${bidResponse.id}?ChatType=Secondary&WithDeleted=true`,
+                await getAuthData(adminId))
+            console.log(getFileAttachmentsListAllFiles)
+            if (getFileAttachmentsListAllFiles.length <= 1) {
+                throw new Error(`количество вернувшихся файлов не корректное`)
+            }
+        })
+    })
+})
+
+test.beforeAll(async () => {
+    await clienApi.getToken(process.env.logistEmail as string, process.env.logistPassword as string);
+});
+
+test.afterAll(async () => {
+    await clienApi.deleteUsedCar(bidInfo.carOption.carId)
+});
--- tests/SystemApi/refuelingTransactions.spec.ts
+++ tests/SystemApi/refuelingTransactions.spec.ts
@@ -4,12 +4,22 @@ import { getAuthData } from '../../database';
 import { BidCreateInfo } from '../../pages/Fixtures';
 import moment from 'moment';
 import APIRequestsClient from '../../api/clienApiRequsets';
+import APIBid from '../../api/bidApi';
+import SupportAPIRequestsClient from '../../api/testSupportRequsets'
 import api from '../../api/apiRequests';
+import DebugAPIRequestsClient from '../../api/debugRequests'
+const emulatorApi = new SupportAPIRequestsClient();
 const clienApi = new APIRequestsClient();
+const bidApi = new APIBid();
+let bidInfo: any;
 const apiUse = new api();
+const debugApi = new DebugAPIRequestsClient();
 const adminId = 1319341 //переделать чтоб доставал из логина в фронте
 test.describe('Создание транзакций по азс', () => {
     let loginPage: LoginPage;
+    let bidResponse: any;
+    let bidInfoResponse: any;
+    let planningRefuelingsArray: any;
     test.beforeEach(async ({ page }) => {
         loginPage = new LoginPage(page);
         await loginPage.goto(); // Переходим на страницу логина перед каждым тестом
@@ -19,6 +29,54 @@ test.describe('Создание транзакций по азс', () => {
         await test.step('Логин', async () => {
             await loginPage.login(process.env.cargorunRefPlanningLogin as string, process.env.cargorunRefPlanningPassword as string);
         });
+        await test.step('Создание заявки и запуск в работу', async () => {
+            const bidFixture = new BidCreateInfo(page);
+            bidInfo = await bidFixture.ApiCommonBid({
+                price: 100000,
+                paymentTypeId: 176,
+                ndsTypeId: 175,
+                planEnterLoadDate: moment().subtract(2, 'd').format('YYYY-MM-DDTHH:mm'),
+                planEnterUnloadDate: moment().add(1, 'd').format('YYYY-MM-DDTHH:mm'),
+                carFilter: `(isDeleted eq false and lastFixedAt le ${moment().subtract(3, 'd').format("YYYY-MM-DDTHH:mm:ss")}.000Z and lastFixedAt le ${moment().subtract(2, 'd').format("YYYY-MM-DDTHH:mm:ss")}.000Z)`,
+                loadAddress: 'Челны',
+                unloadAddress: 'Можга',
+                userIdForFilter: adminId,
+                cargoOwnerFilter: '(isDeleted eq false)'
+            });
+            await bidApi.init();
+            const bidList = await clienApi.GetObjectResponse(
+                `${process.env.url}/api/bids/getlist?$filter=carIds/any(carids:carids in (${bidInfo.carOption.carId}))and ((((status in ('Started')) or (status in ('Planned')))))&$orderby=id desc&$top=30&$skip=0`,
+                await getAuthData(adminId)
+            );
+            bidList.forEach(async (element: any) => {
+                bidApi.cancelBid(element.id, await getAuthData(adminId));
+            });
+            bidResponse = await bidApi.apply(bidInfo, await getAuthData(adminId));
+            await bidApi.setStatus(bidResponse.id, await getAuthData(adminId));
+            await emulatorApi.init();
+            bidInfoResponse = await bidApi.GetBidInfo(bidResponse.id, await getAuthData(adminId));
+            await page.waitForTimeout(15000)
+        });
+        await test.step('Планирование заправок по заявке ', async () => {
+            await page.goto(`${process.env.url}/bids/bid/${bidResponse.id}`)
+            await page.locator("//div[@class='dropdown__btn']").click();
+            await page.locator(`//div[@class="dropdown__item"][contains(text(),'Запланировать заправки')]`).click();
+            await page.locator('input[name="fuelConsumption"]').first().fill('33')
+            await page.locator('input[name="minimumVolume"]').first().fill('200')
+            await page.locator('input[name="currentVolume"]').first().fill('250')
+            await page.locator('input[name="totalVolume"]').first().fill('800')
+            await page.locator('input[name="minimumVolumeInFinishDesired"]').first().fill('750')
+            await page.locator("//div[@class='btn-brand ml-1 btn btn-sm']").click();
+            await expect(page.getByText('Запущен процесс планирования заправок.')).toBeVisible();
+            await page.locator("//div[@class='dropdown__btn']").click();
+            await page.locator(`//div[@class="dropdown__item"][contains(text(),'Перерасчет')]`).click();
+            await page.locator("//div[@class='btn btn-brand btn-sm modal-window__footer-action']").click();
+            // await expect(page.getByText('Ваш запрос выполнен успешно')).toBeVisible();
+            planningRefuelingsArray = await clienApi.GetObjectResponse(
+                `${process.env.url}/api/refueling/getPlannedRefuelings/${bidResponse.id}`,
+                await getAuthData(adminId)
+            );
+        })
         await test.step('отправка запроса', async () => {
             await apiUse.init();
             const patchPrice = await apiUse.postData(`${process.env.url}/api/refuelingTransactions/create`,
@@ -26,22 +84,27 @@ test.describe('Создание транзакций по азс', () => {
                     "transactions": [
                         {
                             "cardNumber": "",
-                            "deviceNumber": "tracker51134838",
-                            "createdAt": "2025-12-01T12:34:23",
-                            "externalId": "1236t23452",
-                            "volume": 123331,
-                            "cost": 123.78,
-                            "description": "важней всего погода в доме",
-                            "address": "dom",
-                            "x": 37.5,
-                            "y": 55.9
+                            "deviceNumber": `${bidInfo.carOption.carTracker}`,
+                            "createdAt": `${moment().subtract(4, 'h').format("YYYY-MM-DDTHH:mm:ss")}`,
+                            "externalId": `${bidInfo.carOption.carTracker}${moment().format("YYYY-MM-DDTHH:mm:ss")}`,
+                            "volume": 300,
+                            "cost": 65.5,
+                            "description": `тест по заявке ${bidResponse.id}`,
+                            "address": `тестовый адресс`,
+                            "x": planningRefuelingsArray.plannedRefuelings[0].mapObject.location.coordinates[1],
+                            "y": planningRefuelingsArray.plannedRefuelings[0].mapObject.location.coordinates[0]
                         }
                     ]
                 }
                 , await getAuthData(adminId))
             console.log(patchPrice)
-        });
+            await debugApi.init();
+            await page.waitForTimeout(5000);
+            await debugApi.runTask('IAnalyzeRefuelingTransactionsReminder', await getAuthData(36))
+            await page.waitForTimeout(5000);
 
+            //TODO доделать завершение вручную и отредачить даты
+        });
     })
 })
 
